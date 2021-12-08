7613483c9e80cfc34427b004b4f239f66572e275
Lucas Walters <lwalters224@gmail.com>
2021-12-07 18:58:08 -0700

    changed to if name..main format

--- rps.py
+++ rps.py
@@ -1,38 +1,46 @@
 import random
 
-# setup
-my_choices = ['rock', 'paper', 'scissors', 'mnms']
-bot_choices = ['rock', 'paper', 'scissors']
+def main():
 
-# game
-while True:
-    my_choice = input("Enter rock, paper, or scissors > ")
+    # setup
+    my_choices = ['rock', 'paper', 'scissors', 'mnms']
+    bot_choices = ['rock', 'paper', 'scissors']
 
-    # check if they want to quit or if they enter something invalid
-    if my_choice == 'quit':
-        quit()
-    elif my_choice not in my_choices:
-        print("You can't choose that!")
-        quit()
+    # game
+    while True:
+        
+        my_choice = input("Enter rock, paper, or scissors > ")
 
-    # bot chooses randomly
-    bot_choice = random.choice(bot_choices)
+        # check if they want to quit or if they enter something invalid
+        if my_choice == 'quit':
+            quit()
 
-    # display what the player and bot chose
-    print(f"You chose: {my_choice}")
-    print(f"The bot chose: {bot_choice}")
+        elif my_choice not in my_choices:
+            print("You can't choose that!")
+            quit()
 
-    # determine if it's a win or loss
-    if my_choice == 'rock' and bot_choice == 'scissors':
-        print("You won!")
-    elif my_choice == 'paper' and bot_choice == 'rock':
-        print("You won!")
-    elif my_choice == 'scissors' and bot_choice == 'paper':
-        print("You won!")
-    elif my_choice == 'mnms':
-        print('You won!')
-        print('MnM\'s always win.')
-    elif my_choice == bot_choice:
-        print("A tie!")
-    else:
-        print("You lost :(")
+        # bot chooses randomly
+        bot_choice = random.choice(bot_choices)
+
+        # display what the player and bot chose
+        print(f"You chose: {my_choice}")
+        print(f"The bot chose: {bot_choice}")
+
+        # determine if it's a win or loss
+        if my_choice == 'rock' and bot_choice == 'scissors':
+            print("You won!")
+
+        elif my_choice == 'paper' and bot_choice == 'rock':
+            print("You won!")
+
+        elif my_choice == 'scissors' and bot_choice == 'paper':
+            print("You won!")
+
+        elif my_choice == bot_choice:
+            print("A tie!")
+
+        else:
+            print("You lost :(")
+
+if __name__ == "__main__":
+    main()
